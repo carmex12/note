2bb378c9867515cb6712d7b6a0fe89daa1e1a647
Justin E <justin.elak@gmail.com>
2026-03-07 20:27:23 -0500

    Store notes.txt in operating system data directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::fs;
 use clap::{Parser, Subcommand};
 use std::fs::{OpenOptions, read_to_string};
 use std::io::Write;
 use std::fs::File;
+use std::path::Path;
+use directories::ProjectDirs;
+
 #[derive(Parser)]
 #[command(version, about = "A simple CLI note app", name = "notes")]
 struct Cli {
@@ -24,35 +28,43 @@ enum Commands{
     Clear
 }
 
-fn add_notes(note: &str) -> std::io::Result<()>{
+fn add_notes(file_directory: &Path, note: &str) -> std::io::Result<()>{
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open("notes.txt")?;
+        .open(file_directory)?;
     writeln!(file, "{note}")?;
     Ok(())
 }
 
-fn clear_notes() -> std::io::Result<()> {
-    File::create("notes.txt")?;
+fn clear_notes(file_directory: &Path) -> std::io::Result<()> {
+    File::create(file_directory)?;
     Ok(())
 }
 
-fn list_notes() {
-    let contents = read_to_string("notes.txt")
+fn list_notes(file_directory: &Path) {
+    let contents = read_to_string(file_directory)
         .expect("Couldnt read note.txt");
     println!("{contents}")
 }
 
 fn main() {
     let cli = Cli::parse();
+    let project_dirs = ProjectDirs::from("com", "JustinE", "note")
+        .expect("Could not find a home directory");
+
+    let data_dir = project_dirs.data_dir();
+    fs::create_dir_all(data_dir)
+        .expect("Could not create app directory");
+
+    let directory = data_dir.join("notes.txt");
 
     match cli.command {
         Commands::Add { text } => {
             let note = text.join(" ");
-            add_notes(&note).expect("Error with adding notes to notes.txt");
+            add_notes(&directory, &note).expect("Error with adding notes to notes.txt");
         }
-        Commands::List => list_notes(),
-        Commands::Clear => clear_notes().expect("Error with clearing notes.txt")
+        Commands::List => list_notes(&directory),
+        Commands::Clear => clear_notes(&directory).expect("Error with clearing notes.txt")
     }
 }
